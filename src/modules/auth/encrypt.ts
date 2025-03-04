e6527b2c19651d35c729275d4d891d96e56d6895
Shafee Muhsin <shafeem@fortude.co>
2025-03-04 21:15:08 +0530

    clean code

--- src/modules/auth/encrypt.ts
+++ src/modules/auth/encrypt.ts
@@ -9,10 +9,6 @@ dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET || "secret-key";
 
 export class encrypt {
-  static async encryptpass(password: string) {
-    return bcrypt.hashSync(password, 12);
-  }
-
   static async verifyPassword(hashPassword: string, password: string) {
     return await bcrypt.compare(password, hashPassword);
   }
