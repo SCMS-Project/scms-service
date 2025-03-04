453b05775d476d6a101e1b50dba699e328f6cc5c
Shafee Muhsin <shafeem@fortude.co>
2025-03-04 16:13:25 +0530

    log update

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,6 +6,7 @@ dotenv.config();
 const MONGO_URI: any = process.env.MONGO_URI;
 
 const connectMongoDB = async () => {
+  console.log("Connecting MongoDB database...");
   try {
     await mongoose.connect(MONGO_URI);
     console.log("MongoDB successfully connected.");
