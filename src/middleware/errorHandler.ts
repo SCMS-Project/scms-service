9a8fdced12d94ff340b0cd114b301e4a79200e61
Shafee Muhsin <shafeem@fortude.co>
2025-03-08 13:27:32 +0530

    code clean

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+
 import HttpException from "../util/http-exception.model";
 
 export const errorHandler = (
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -9,8 +9,7 @@ router.post(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const user = await signup({ ...req.body.user });
-
-      res.status(201).json({ ...user });
+      res.status(201).json(user);
     } catch (error: any) {
       next(error);
     }
@@ -22,7 +21,7 @@ router.post(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       console.log({ ...req.body });
-      const result = await login({ ...req.body });
+      const result = await login(req.body);
       res.status(200).json(result);
     } catch (error) {
       next(error);
